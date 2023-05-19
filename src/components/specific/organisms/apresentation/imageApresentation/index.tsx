dd6dec7822483bff56eb266e78b2d33aabf4bf3e
aldcejam <aldcejamj923@gmail.com>
2023-05-19 00:24:59 -0300

    all

--- src/components/specific/organisms/apresentation/imageApresentation/index.tsx
+++ src/components/specific/organisms/apresentation/imageApresentation/index.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image"
+import styled from "./styled.module.scss"
 
 export const ImageApresentation = () => {
     return (
         <div className="w-full h-full">
             <div className="absolute bottom-0 w-[40%] max-w-[350px]">
-                <Image width={"645"} height={"969"} style={{ objectFit: "contain" }} alt="foto de aldcejam" src={"/aldcejam.png"} />
+                <Image className={styled["image-shadow"]} width={"645"} height={"969"} style={{ objectFit: "contain" }} alt="foto de aldcejam" src={"/aldcejam.png"} />
             </div>
-
         </div>
     )
 }
